b725b245086cb0dfeca456ad4edc3503c7e15a1b
JusttRelaxx <miroslav12122@gmail.com>
2014-10-11 20:41:26 +0300

    Problem4 solved

---
+++ week0/week0-file_system_problems/F4/sum_integers.py
@@ -0,0 +1,27 @@
+
+from sys import argv
+
+
+def sum_integers_from_file(filename):
+    
+    content = open(filename, "r")
+
+    integers_Sum = 0
+    for line in content:
+        integers = line.split()
+        for integer in integers:
+            integers_Sum += int(integer)
+    content.close()
+    return integers_Sum
+
+
+
+def main():
+
+    filename = argv[1]
+    print(sum_integers_from_file(filename))
+
+
+
+if __name__ == '__main__':
+    main()
