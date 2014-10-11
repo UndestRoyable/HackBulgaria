839c8be96d62ca7ba1f4e1c1f955ffedde9d24df
JusttRelaxx <miroslav12122@gmail.com>
2014-10-11 20:15:29 +0300

    Problem3 - solved

--- week0/week0-file_system_problems/F3/generate_numbers.py
+++ week0/week0-file_system_problems/F3/generate_numbers.py
@@ -1,25 +1,19 @@
-from random import randint
 import sys
+from random import randint
 
 
 def main():
-    file_name = sys.argv[1]
+
+    file_to_write = sys.argv[1]
+    randInt_count = 0
     randInt_count = int(sys.argv[2])
-    content = []
+
+    file = open(file_to_write, "w")
     for i in range(randInt_count):
-        content.append(randint(1,1000))
-
-    open_file = open(file_name,"w")
-    open_file.write(str(content))
-    file_content = open_file.read()
-    print(file_content)
-
+        rand_number = randint(0, 1001)
+        file.write(str(rand_number))
+        file.write("\n")
     file.close()
-
-# FIX
 
 if __name__ == '__main__':
     main()
-
-
-    
