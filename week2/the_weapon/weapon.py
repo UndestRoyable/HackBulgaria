0162e0715dc70f6eb7dcc9d114151a3f4513ebc9
JusttRelaxx <miroslav12122@gmail.com>
2014-10-20 21:09:04 +0300

    Weapon done.

---
+++ week2/the_weapon/weapon.py
@@ -0,0 +1,13 @@
+import random
+class Weapon:
+
+    def __init__(self, type, damage, critical_strike_percent):
+        self.type = type
+        self.damage = damage
+        self.critical_strike_percent = critical_strike_percent
+
+    def critical_hit(self):
+        hit = random.random()
+        if hit <= self.critical_strike_percent:
+            return True
+        return False
---
+++ week2/the_weapon/weapon_test.py
@@ -0,0 +1,24 @@
+import unittest
+from weapon import Weapon 
+class WeaponTest (unittest.TestCase):
+    
+    def setUp(self):
+        self.weapon = Weapon("Mighty Axe", 25, 0.2)
+
+    def test_weapon_init(self):
+
+        self.assertEqual(self.weapon.type, "Mighty Axe")
+        self.assertEqual(self.weapon.damage, 25)
+        self.assertEqual(self.weapon.critical_strike_percent, 0.2)
+
+    def test_critical_hit(self):
+        
+        self.assertTrue(self.weapon.critical_hit())
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
