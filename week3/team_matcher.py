fd74d91a059261a57bfd8251cfb7e12866614898
JusttRelaxx <miroslav12122@gmail.com>
2014-10-27 20:16:09 +0200

    team_matcher working! In command_parser implement user choosing for the function

--- week3/team_matcher.py
+++ week3/team_matcher.py
@@ -1,21 +1,72 @@
 import requests
 import json
+from random import shuffle
+
+api_data = requests.get("https://hackbulgaria.com/api/students/", verify = False)
+api_data_json = api_data.json()
+
+def menu_message():
+    print("Hello, you can use one the the following commands:")
+    print("list_courses - this lists all the courses that are available now.")
+    print("match_teams <course_id>, <team_size>, <group_time>")
+    print("-----------------------------------------")
+
 
 def list_all_courses():
-    api_data = requests.get("https://hackbulgaria.com/api/students/", verify = False)
-    #return api_data.status_code
-    api_data_json = api_data.json()
-    courses = set()
-    for dictionary in api_data_json:
-        for course in dictionary["courses"]:
-            courses.add(course["name"])
+    
+    courses = []
+    for item in api_data_json:
+        for course in item["courses"]:
+            if course["name"] not in courses:
+                courses.append(course["name"])
 
-    return courses
+    print("HackBulgaria's courses:")
+    counter = 0
+
+    for course in courses:
+        counter += 1 
+        print("[{}]".format(counter) + course)
 
 
+def match_teams(course_id, team_size, group_time):
+
+    hack_courses = []
+    for item in api_data_json:
+        for course in item["courses"]:
+            if course["name"] not in hack_courses:
+                hack_courses.append(course["name"])
+
+    course_wanted = hack_courses[course_id]
+    persons = []
+
+    for person in api_data_json:
+        for course in person["courses"]:
+            if course["name"] == course_wanted and course["group"] == group_time:
+                persons.append(person["name"])
+                break
+    
+    broq4 = 0 #MUAHAHAHAHAHA, FUCK QUALITY CODE 
+    shuffle(persons)
+    for i in range(len(persons)):
+        if broq4 == team_size:
+            print("=========================")
+            broq4 = 0
+        print (persons[i])
+        broq4 += 1
+
+def command_parser():
+    command = input("Enter command:")
+    if command == "list":
+        list_all_courses()
+    else:
+        raise ValueError("Invalid command! Available commands: [list] and [match_teams <course_id>, <team_size>, <group_time>] ")
 
 def main():
-    print(list_all_courses())
+    print(menu_message())
+    
+    print(command_parser())
+    
+    print(match_teams(3, 2, 2)) # In command_parser implement user choosing for the function
 
 if __name__ == '__main__':
     main()
